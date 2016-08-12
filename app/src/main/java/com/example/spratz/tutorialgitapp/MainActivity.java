e26002a0384377ba79ee8695d4685009f368d0d6
spratz <Salvador.Pratz@turner.com>
2016-08-12 11:52:12 -0400

    Add button

--- app/src/main/java/com/example/spratz/tutorialgitapp/MainActivity.java
+++ app/src/main/java/com/example/spratz/tutorialgitapp/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.spratz.tutorialgitapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ActionMenuItemView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.bntMessage);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "message", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
+
 }
